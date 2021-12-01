1dc35360b4362ba34ef12e27c35f640a42946232
Alex Perez <apramos89@gmail.com>
2021-12-01 16:22:55 +0100

    Prepair for day 2 :raised_hands:

---
+++ day-2/src/main.rs
@@ -0,0 +1,18 @@
+fn solve_part1(input: &str) -> i32 {
+    0
+}
+
+fn solve_part2(input: &str) -> i32 {
+    0
+}
+
+fn main() {
+    let input = include_str!("../input.txt");
+
+    let solution1 = solve_part1(input);
+
+    let solution2 = solve_part2(input);
+
+    println!("PART 1: {}", solution1);
+    println!("PART 2: {}", solution2);
+}
