ad429c8edab79e68f2adf65dbc5ebc918caf488d
Alex Perez <apramos89@gmail.com>
2021-12-04 18:02:55 +0100

    Day 5 template

---
+++ day-5/src/main.rs
@@ -0,0 +1,25 @@
+fn solve_part1(input: &Vec<i32>) -> i32 {
+    0
+}
+
+fn solve_part2(input: &Vec<i32>) -> i32 {
+    0
+}
+
+fn prepare_input() -> Vec<i32> {
+    include_str!("../input.txt")
+        .lines()
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect()
+}
+
+fn main() {
+    let input = prepare_input();
+
+    let solution1 = solve_part1(&input);
+
+    let solution2 = solve_part2(&input);
+
+    println!("PART 1: {}", solution1);
+    println!("PART 2: {}", solution2);
+}
