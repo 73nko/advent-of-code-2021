efff1a06cf5704cd71b95cfa75cda748d186d8bc
Alex Perez <apramos89@gmail.com>
2021-12-05 10:34:43 +0100

    Day 4 refactor code

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, num::ParseIntError, str::FromStr};
+use std::convert::From;
 
 #[derive(Debug, Clone)]
 struct Cell {
@@ -17,20 +17,20 @@ impl Cell {
 
 #[derive(Debug, Clone)]
 struct Board {
-    data: Vec<Cell>,
+    numbers: Vec<Cell>,
 }
 
 impl Board {
     const LINE_SIZE: usize = 5;
 
     fn new_number(&mut self, value: u32) {
-        if let Some(c) = self.data.iter_mut().find(|c| c.value == value) {
+        if let Some(c) = self.numbers.iter_mut().find(|c| c.value == value) {
             c.marked = true;
         }
     }
 
     fn has_line(&self) -> bool {
-        for row in self.data.chunks(Self::LINE_SIZE) {
+        for row in self.numbers.chunks(Self::LINE_SIZE) {
             if row.iter().all(|cell| cell.marked) {
                 return true;
             }
@@ -42,7 +42,7 @@ impl Board {
     fn has_column(&self) -> bool {
         for column in 0..Self::LINE_SIZE {
             if self
-                .data
+                .numbers
                 .iter()
                 .skip(column)
                 .step_by(Self::LINE_SIZE)
@@ -60,7 +60,7 @@ impl Board {
     }
 
     fn sum_non_marked(&self) -> u32 {
-        self.data
+        self.numbers
             .iter()
             .filter(|c| !c.marked)
             .map(|c| c.value)
@@ -68,16 +68,15 @@ impl Board {
     }
 }
 
-impl FromStr for Board {
-    type Err = ParseIntError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let d: Result<Vec<Cell>, _> = s
+impl From<&str> for Board {
+    fn from(s: &str) -> Self {
+        let numbers: Vec<Cell> = s
             .split_whitespace()
-            .map(|s| s.parse().map(Cell::new))
+            .map(|s| s.parse().unwrap())
+            .map(Cell::new)
             .collect();
 
-        Ok(Board { data: d? })
+        Board { numbers }
     }
 }
 
@@ -98,12 +97,15 @@ fn solve_part1((values, boards): &(Vec<u32>, Vec<Board>)) -> u32 {
 fn solve_part2((values, boards): &(Vec<u32>, Vec<Board>)) -> u32 {
     let mut boards = boards.clone();
     let n_boards = boards.len();
-    let mut winners = HashSet::new();
+    let mut winners = Vec::new();
     for value in values {
         for (index, board) in boards.iter_mut().enumerate() {
+            if winners.contains(&index) {
+                continue;
+            }
             board.new_number(*value);
             if board.is_winner() {
-                winners.insert(index);
+                winners.push(index);
                 if winners.len() == n_boards {
                     return board.sum_non_marked() * value;
                 }
@@ -118,12 +120,7 @@ fn prepare_input() -> (Vec<u32>, Vec<Board>) {
     let (first_line, boards) = include_str!("../input.txt").split_once('\n').unwrap();
 
     let values: Vec<u32> = first_line.split(',').map(|s| s.parse().unwrap()).collect();
-
-    let boards: Vec<Board> = boards
-        .trim_start()
-        .split("\n\n")
-        .map(|f| f.parse().unwrap())
-        .collect();
+    let boards: Vec<Board> = boards.trim_start().split("\n\n").map(Board::from).collect();
 
     (values, boards)
 }
