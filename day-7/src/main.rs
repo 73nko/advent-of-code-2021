2c82222fe0b28408688b93f621984f60b00bcb4c
Alex Perez <apramos89@gmail.com>
2021-12-07 09:44:34 +0100

    Day 7 finished. I dont know why

--- day-7/src/main.rs
+++ day-7/src/main.rs
@@ -1,9 +1,26 @@
-fn solve_part1(input: Vec<i32>) -> i32 {
-    0
+fn solve_part1(input: &Vec<i32>) -> i32 {
+    let mut crabs = input.clone();
+    let middle = input.len() / 2;
+
+    crabs.select_nth_unstable(middle);
+    crabs.iter().map(|crap| (*crap - crabs[middle]).abs()).sum()
 }
 
-fn solve_part2(input: Vec<i32>) -> i32 {
-    0
+fn calc(target: i32, crabs: &Vec<i32>) -> usize {
+    crabs
+        .iter()
+        .map(|crab| {
+            let crab_mean = (*crab - target).abs();
+            ((crab_mean * (crab_mean + 1)) / 2) as usize
+        })
+        .sum()
+}
+
+fn solve_part2(input: &Vec<i32>) -> usize {
+    let crabs = input.clone();
+    let average: f64 = crabs.iter().map(|c| *c as f64).sum::<f64>() / crabs.len() as f64;
+
+    calc(average.floor() as i32, &crabs)
 }
 
 fn prepare_input() -> Vec<i32> {
@@ -16,9 +33,8 @@ fn prepare_input() -> Vec<i32> {
 fn main() {
     let input = prepare_input();
 
-    let solution1 = solve_part1(input.clone());
-
-    let solution2 = solve_part2(input.clone());
+    let solution1 = solve_part1(&input);
+    let solution2 = solve_part2(&input);
 
     println!("PART 1: {}", solution1);
     println!("PART 2: {}", solution2);
